e85552b91b0a07c01df7f91a05a31379934e26d4
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2022-01-31 21:07:42 -0600

    staring transform

---
+++ USACO-CPP/transform.cpp
@@ -0,0 +1,25 @@
+/*
+ID: bliscos1
+TASK: transform
+LANG: C++
+*/
+/* LANG can be C++11 or C++14 for those more recent releases */
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ofstream fout("milk2.out");
+    ifstream fin("milk2.in");
+    int n;
+    vector<vector<char>> vOrig, vTrans;
+
+    fin >> n;
+
+
+    return 0;
+}
